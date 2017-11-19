f20c541a9c1ef9df3960a171aa04ce2bb38d9ba1
Eric Stansifer <eric.stansifer+git@gmail.com>
2017-11-19 05:40:53 -0500

    Can choose players on command line

--- players.py
+++ players.py
@@ -1,5 +1,14 @@
 import playrandom
 import playhuman
 
-random = lambda white : playrandom.AIRandom(white)
-human = lambda white : playhuman.Human(white)
+random = playrandom.AIRandom
+human = playhuman.Human
+
+players = [human, random]
+
+def lookup_player_by_name(name):
+    name = name.lower()
+    if name == 'human':
+        return human
+    if name in ['random', 'airandom']:
+        return random
--- run.py
+++ run.py
@@ -1,10 +1,27 @@
+import random
+import sys
 import game
 import players
 
-def run():
+def play_game(white, black):
     g = game.Game()
-    g.set_players(players.human(True), players.random(False))
+    g.set_players(white(True), black(False))
     g.play()
+
+def run():
+    if len(sys.argv) == 1:
+        p = [players.human, players.random]
+        random.shuffle(p)
+        play_game(p[0], p[1])
+    if len(sys.argv) == 3:
+        white = players.lookup_player_by_name(sys.argv[1])
+        black = players.lookup_player_by_name(sys.argv[2])
+        if white is None:
+            print("Don't recognize player " + sys.argv[1])
+        if black is None:
+            print("Don't recognize player " + sys.argv[2])
+        if not ((white is None) or (black is None)):
+            play_game(white, black)
 
 if __name__ == "__main__":
     run()
